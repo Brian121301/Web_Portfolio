71f532abe1f5698214724f4db5475a8e73fc7fd5
Brian121301 <briannajera3@gmail.com>
2024-07-17 23:28:17 -0500

    updated about me
    - added micro interaction to skills
    -added image

--- portfolio/src/app/about/page.js
+++ portfolio/src/app/about/page.js
@@ -2,41 +2,49 @@ import Skills from "./techs";
 
 const About = () => {
     return (
-        <div className="min-h-screen bg-blue-100 pt-20">
-            <div className="max-w-6xl mx-auto bg-white shadow-lg rounded-lg">
-                <div className="p-8">
-                    <h1 className="text-4xl md:text-6xl font-bold text-center mb-8 text-gray-800">Who Am I?</h1>
-                    <div className="text-lg space-y-6 text-gray-700 leading-relaxed">
+        <div className="min-h-screen bg-blue-100">
+            <div className="flex items-center justify-center">
+                <div className="flex justify-center ml-20 p-20">
+                    <img
+                        src="brian_img.png"
+                        alt="Brian Najera"
+                        className="h-76 w-72 object-contain"
+                    />
+                </div>
+                <div className="max-w-6xl mx-auto rounded-lg">
+                    <h1 className="text-4xl md:text-6xl font-bold text-center mb-8 text-gray-800 pt-8">Who Am I?</h1>
+                    <div className="text-lg space-y-6 text-gray-700 leading-relaxed p-8">
                         <p>
-                            Hi, I'm Brian Najera, a passionate programmer based in Fort Worth, TX, and a 
-                            recent Computer Science graduate from the University of North Texas. I thrive 
+                            Hi, I'm Brian Najera, a passionate programmer based in Fort Worth, TX, and a
+                            recent Computer Science graduate from the University of North Texas. I thrive
                             on challenges and am always eager to learn new skills.
                         </p>
                         <p>
-                            I have experience with Python, JavaScript, C/C++, HTML/CSS, SQL, and PHP, and 
-                            am proficient with tools like Visual Studio Code, PyCharm, IntelliJ, and frameworks 
-                            such as React Native and React.js. My technical background includes certifications 
-                            in Cybersecurity.
+                            I have experience with many languages such as Python, SQL, and JavaScript and am also proficient
+                            with tools like Visual Studio Code, IntelliJ, and frameworks such as React
+                            Native and React.js. My technical background includes a certification in Cybersecurity.
                         </p>
                         <p>
-                            I've worked on notable projects, including a mobile app for improving law enforcement 
-                            response during active shooter incidents and an e-commerce website designed to implement 
-                            cybersecurity challenges in a real-world context. These experiences have honed my 
+                            I've worked on notable projects, including a mobile app for improving law enforcement
+                            response during active shooter incidents and an e-commerce website designed to implement
+                            cybersecurity challenges in a real-world context. These experiences have honed my
                             problem-solving skills and adaptability.
                         </p>
                         <p>
-                            Currently, as a Team Manager at Pilot Flying J, I lead and manage a team, ensuring 
-                            efficient operations and a positive work environment. In my free time, I enjoy learning 
+                            Currently, as a Team Manager at Pilot Flying J, I lead and manage a team, ensuring
+                            efficient operations and a positive work environment. In my free time, I enjoy learning
                             new coding skills and embarking on new projects.
                         </p>
                         <p>
-                            I am particularly interested in web and iOS development and am excited about opportunities 
+                            I am particularly interested in web and iOS development due to the great deal of enjoyment i received from working
+                            on the I.M.R.S app and the Cybersecurity website. I am excited about opportunities
                             to grow and contribute to innovative projects in these fields.
                         </p>
                     </div>
                 </div>
             </div>
-            <div className="max-w-6xl mx-auto mt-8 bg-gray-200 p-8 rounded-lg shadow-lg">
+
+            <div className="ml-44 mr-44">
                 <Skills />
             </div>
         </div>
--- portfolio/src/app/about/techs.js
+++ portfolio/src/app/about/techs.js
@@ -1,6 +1,10 @@
+"use client";
 import React from 'react';
 import { CgCPlusPlus } from "react-icons/cg";
-import { FaDatabase } from 'react-icons/fa';
+import {
+    FaDatabase,
+    FaCode,
+} from 'react-icons/fa';
 import {
     DiJavascript1,
     DiReact,
@@ -14,7 +18,7 @@ import {
     SiIntellijidea,
     SiMacos,
     SiPycharm,
-  } from "react-icons/si";
+} from "react-icons/si";
 
 const skills = [
     { name: 'C++', icon: CgCPlusPlus },
@@ -23,6 +27,7 @@ const skills = [
     { name: 'Node.js', icon: DiNodejs },
     { name: 'Python', icon: DiPython },
     { name: 'SQL', icon: FaDatabase },
+    { name: 'HTML & CSS', icon: FaCode },
     { name: 'Git', icon: DiGit },
     { name: 'Java', icon: DiJava },
 ];
@@ -32,32 +37,44 @@ const tools = [
     { name: 'MacOS', icon: SiMacos },
     { name: 'IntelliJ IDEA', icon: SiIntellijidea },
     { name: 'PyCharm', icon: SiPycharm },
-]
+];
 
 const Skills = () => {
+    const [activeSkill, setActiveSkill] = React.useState(null);
+
+    const handleMouseEnter = (index) => {
+        setActiveSkill(index);
+    };
+
+    const handleMouseLeave = () => {
+        setActiveSkill(null);
+    };
+
     return (
         <div className="text-center">
             <div>
-                <h2 className="text-4xl md:text-6xl font-bold mb-4 text-gray-800">Skills</h2>
-                <div className="grid grid-cols-3 gap-4">
+                <h2 className="text-4xl md:text-6xl font-bold mb-4 text-gray-800 p-4">Skills</h2>
+                <div className="grid grid-cols-4 gap-8">
                     {skills.map((skill, index) => (
-                        <div key={index} className="flex flex-col items-center">
-                            {React.createElement(skill.icon, { className: 'w-12 h-12 md:w-16 md:h-16 text-blue-500' })}
-                            <span className="text-lg md:text-xl text-gray-700 mt-2">{skill.name}</span>
+                        <div key={index} className="flex flex-col items-center justify-center shadow-lg outline outline-4 outline-blue-500 pt-4 hover:bg-white hover:outline-white hover:shadow-xl transition duration-500 ease-in-out"
+                            onMouseEnter={() => handleMouseEnter(index)}
+                            onMouseLeave={handleMouseLeave}>
+                            {React.createElement(skill.icon, { className: 'w-20 h-24 text-blue-500' })}
+                            <span className={`text-lg md:text-xl text-gray-700 mt-2 ${activeSkill === index ? 'opacity-100' : 'opacity-0'}`}>{skill.name}</span>
                         </div>
                     ))}
                 </div>
             </div>
             <div>
-            <h2 className="text-4xl md:text-6xl font-bold mb-4 text-gray-800">Tools</h2>
-            <div className="grid grid-cols-3 gap-4">
-                {tools.map((skill, index) => (
-                    <div key={index} className="flex flex-col items-center">
-                        {React.createElement(skill.icon, { className: 'w-12 h-12 md:w-16 md:h-16 text-blue-500' })}
-                        <span className="text-lg md:text-xl text-gray-700 mt-2">{skill.name}</span>
-                    </div>
-                ))}
-            </div>
+                <h2 className="text-4xl md:text-6xl font-bold mb-4 text-gray-800 pt-20">Tools</h2>
+                <div className="grid grid-cols-3 gap-4">
+                    {tools.map((skill, index) => (
+                        <div key={index} className="flex flex-col items-center justify-center">
+                            {React.createElement(skill.icon, { className: 'w-12 h-12 md:w-16 md:h-16 text-blue-500' })}
+                            <span className="text-lg md:text-xl text-gray-700 mt-2">{skill.name}</span>
+                        </div>
+                    ))}
+                </div>
             </div>
         </div>
     );
